e03650d9cb220e648cc09cf3cdbbc8777ef66eb5
Anthony Machula <x@anthonymachula.ca>
2024-07-08 16:34:27 -0400

    updated config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build'
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    base: isProduction ? '/web-brdi/' : '/',  // Use your repository name here
+  }
 })
